cdc90158fb3951d8f6312ff4b43e8fa0cccfd8eb
L-E-W-1-5 <lewiswootton88@hotmail.com>
2022-12-09 11:56:23 +0000

    useEffect

--- christmasapp/src/App.js
+++ christmasapp/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import {useState, useEffect} from 'react'
 import {PresentList} from './PresentList/PresentList.js'
 import {Navbar} from './Navbar/Navbar.js'
 import {Header} from './Header/Header.js'
 
+const url = 'localhost:3000'
+
 function App() {
+  const [people, setPeople] = useState([])
+  
+
+  useEffect(() => {
+    fetch(`${url}`, {
+      headers: { Accept: 'application/json' },
+    })
+      .then((res) => res.json())
+      .then(({response}) => setPeople(response))
+      
+  }, [])
+
   return (
     <div className="App">
       <>
         <Navbar></Navbar>
         <Header></Header>
-        <PresentList></PresentList>
+        <PresentList people={people}></PresentList>
       </>
     </div>
   );
--- christmasapp/src/Navbar/Navbar.js
+++ christmasapp/src/Navbar/Navbar.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
 import {AddPresent} from '../AddPresent/AddPresent.js'
-export function Navbar() {
+
+
+export function Navbar(props) {
+
     const [formVis, setVis] = useState(false)
     const [value, setValue] = useState()
+
     // useEffect(()=>{
     //     const response = await fetch("localhost:3000/value")
     //     const data = await response.json()
     //     return data
     // },[value])
+
     return (
         <>
         <div style={{visibility: formVis ? "visible" : "hidden"}}>
             <AddPresent></AddPresent>
         </div>
-        <button onClick={()=>{setVis(current=>!current)}}>Add present</button>
-        <select onChange={(e)=>{setValue(e.target.value)}} value={value}>
-            <option value="name1">1</option>
-            <option value="name2">2</option>
-            <option value="name3">3</option>
-        </select>
-        <>
 
-        </>
+        <button onClick={()=>{setVis(current=>!current)}}>Add present</button>
+
+        <select onChange={(e)=>{setValue(e.target.value)}} value={value}>
+
+            {props.people.map((person) => {
+                return <option value={person.name}>{person.name}</option>
+            })}
+
+        </select>
         </>
     )
 }
